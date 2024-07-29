caac6250a53c5a75676f2019dad4656de5b12aae
Sudipta Singha <singhaashoka03@gmail.com>
2024-07-29 21:58:10 +0530

    Test 2

--- tata-innovent/src/components/App.jsx
+++ tata-innovent/src/components/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   })
 
   return (
-    <Route basename="/inov">
+    <Route>
       <Header user={user} setUser={setUser} />
       <Main user={user} setUser={setUser}
         remember={remember} setRemember={setRemember}
