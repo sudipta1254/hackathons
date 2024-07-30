821ab52fadc79a5c7ac5fd73f6161e9bb2c12707
Sudipta Singha <singhaashoka03@gmail.com>
2024-07-30 12:38:00 +0530

    Upgraded chatbot to multichat

--- tata-innovent/src/components/genAI/Chatbot.jsx
+++ tata-innovent/src/components/genAI/Chatbot.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom"
-import spark from "../../assets/spark.svg"
 import M from "materialize-css"
 import { handleEnter } from '../../utils/handleEnter'
 import "materialize-css/dist/css/materialize.min.css"
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import MarkDown from "react-markdown"
+import spark from "../../assets/spark.svg"
 import $ from "jquery"
 import "../../index.css"
 
@@ -15,10 +15,11 @@ const Chatbot = ({user, setUser}) => {
    document.title = "personaAI - AI"
    const navigate = useNavigate()
    
-   const [input, setInput] = useState(null)
-   const [data, setData] = useState(null)
-   const [error, setError] = useState(null)
+   const [input, setInput] = useState("")
+   const [data, setData] = useState("")
+   const [error, setError] = useState("")
    const [loading, setLoading] = useState(false)
+   const [history, setHistory] = useState([])
 
    useEffect(() => {
       navigate(!user?.logged && "/")
@@ -30,6 +31,15 @@ const Chatbot = ({user, setUser}) => {
       $(".Chatbot-btn").text(loading ? "cached" : "send")
       .toggleClass("chatbot-btn-rotate")
    }, [loading])
+   useEffect(() => {
+      setHistory([...history, {
+         role: "user",
+         parts: [{ text: input }],
+      }, {
+         role: "model",
+         parts: [{ text: data }],
+      }])
+   }, [data])
    
    const generateContent = async() => {
       if (!input) {
@@ -39,13 +49,21 @@ const Chatbot = ({user, setUser}) => {
       setLoading(true)
       try {
          const model = genai.getGenerativeModel({ model: "gemini-1.5-flash"});
-         const result = await model.generateContent(input);
+         const chat = model.startChat({
+            history: history,
+            generationConfig: {
+            //   maxOutputTokens: 100,
+            },
+         });
+      
+         const result = await chat.sendMessage(input);
          const response = result.response;
          const text = response.text();
-         console.log(text)
+         console.log(text);
          setData(text)
          setError(null)
          setLoading(false)
+         setInput("")
       } catch (err) {
          console.log(err)
          setData(null)
@@ -61,15 +79,26 @@ const Chatbot = ({user, setUser}) => {
          </div>
          <div className="data-container">
             { !data && <div className="dummyText">
-               <h4>This is a dummy text</h4>
+               <img src={spark} alt="spark" style={{width:"4rem"}} />
+               <div className="dummy-options">
+                  <div className="option-1 option">
+                     <i className="material-icons">add</i>
+                     <p>Option 1</p>
+                  </div>
+                  <div className="option-2 option">
+                     <i className="material-icons">add</i>
+                     <p>Option 2</p>
+                  </div>
+               </div>
             </div> }
             <MarkDown>{ data }</MarkDown>
          </div>
          <div className="input-container">
             <i className="material-icons">attach_file</i>
             <input id="Chatbot-input" type="text" placeholder="Enter prompt.."
+               value={input}
                onKeyDown={e => handleEnter(e, ".Chatbot-btn") }
-               onChange={e => setInput(e.target.value?.trim()) }
+               onChange={e => setInput(e.target.value) }
             />
             <i className="material-icons Chatbot-btn" type="button"
                onClick={ generateContent }
